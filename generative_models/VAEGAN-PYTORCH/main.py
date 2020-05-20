464bc1587fdf707704fd020044c0e6b18d0e3dea
Rishabh Dugaye <58358984+rishabhd786@users.noreply.github.com>
2020-05-20 11:02:20 +0530

    Update main.py

--- generative_models/VAEGAN-PYTORCH/main.py
+++ generative_models/VAEGAN-PYTORCH/main.py
@@ -5,28 +5,70 @@ import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.autograd import Variable
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-torch.autograd.set_detect_anomaly(True)
+
+import os
+import time
+from collections import OrderedDict
+import logging
+import argparse
+
 from dataloader import dataloader
 from models import VAE_GAN,Discriminator
 from utils import show_and_save,plot_loss
 
-data_loader=dataloader(64)
-gen=VAE_GAN().to(device)
-discrim=Discriminator().to(device)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch.autograd.set_detect_anomaly(True)
+
+parser = argparse.ArgumentParser()
+    # model config
+    parser.add_argument('--image_size', type=int, default=64)
+    parser.add_argument('--latent_dim', type=int, default=128)
+    # run config
+    parser.add_argument('--ndata', type=str, required=True)
+    parser.add_argument('--root', type=str, default=../data)
+    # optim config
+    parser.add_argument('--epochs', type=int, default=25)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--lr', type=float, default=0.0003)
+    parser.add_argument('--alpha', type=float, default=0.1)
+    parser.add_argument('--beta', type=int, default=5)
+    parser.add_argument('--gamma', type=float, default=15)
+
+    args = parser.parse_args()
+
+batch_size=args.batch_size
+latent_dim=args.latent_dim
+
+if args.ndata=='mnist':
+  in_channels=1
+  out_channels=1
+if args.ndata=='cifar10':
+  in_channels=3
+  out_channels=3 
+
+data_loader=dataloader(args.batch_size,args.root,args.ndata,args.image_size)
+
+gen=VAE_GAN(in_channels,out_channels,latent_dim).to(device)
+discrim=Discriminator(in_channels).to(device)
+
 real_batch = next(iter(data_loader))
 show_and_save("training" ,make_grid((real_batch[0]*0.5+0.5).cpu(),8))
 
-epochs=25
-lr=3e-4
-alpha=0.1
-gamma=15
+#hyperparameters
+epochs=args.epochs
+lr=args.lr
+alpha=args.alpha
+beta=args.beta
+gamma=args.gamma
+
+
 
 criterion=nn.BCELoss().to(device)
 optim_E=torch.optim.RMSprop(gen.encoder.parameters(), lr=lr)
 optim_D=torch.optim.RMSprop(gen.decoder.parameters(), lr=lr)
 optim_Dis=torch.optim.RMSprop(discrim.parameters(), lr=lr*alpha)
-z_fixed=Variable(torch.randn((64,128))).to(device)
+
+z_fixed=Variable(torch.randn((batch_size,latent_dim))).to(device)
 x_fixed=Variable(real_batch[0]).to(device)
 
 for epoch in range(epochs):
@@ -36,13 +78,15 @@ for epoch in range(epochs):
     bs=data.size()[0]
     
     ones_label=Variable(torch.ones(bs,1)).to(device)
-    zeros_label=Variable(torch.zeros(bs,1)).to(device)
-    zeros_label1=Variable(torch.zeros(64,1)).to(device)
+    zeros_label=Variable(torch.zeros(bs,1)).to(device)        #ones and zeros labels for real and fake data
+    zeros_label1=Variable(torch.zeros(batch_size,1)).to(device)
+    
     datav = Variable(data).to(device)
     mean, logvar, rec_enc = gen(datav)
-    z_p = Variable(torch.randn(64,128)).to(device)
+    z_p = Variable(torch.randn(batch_size,latent_dim)).to(device)
     x_p_tilda = gen.decoder(z_p)
- 
+   
+    # Training Discriminator
     output = discrim(datav)[0]
     errD_real = criterion(output, ones_label)
     dis_real_list.append(errD_real.item())
@@ -58,7 +102,7 @@ for epoch in range(epochs):
     gan_loss.backward(retain_graph=True)
     optim_Dis.step()
 
-
+    #Training Decoder
     output = discrim(datav)[0]
     errD_real = criterion(output, ones_label)
     output = discrim(rec_enc)[0]
@@ -70,21 +114,22 @@ for epoch in range(epochs):
 
     x_l_tilda = discrim(rec_enc)[1]
     x_l = discrim(datav)[1]
-    rec_loss = ((x_l_tilda - x_l) ** 2).mean()
+    rec_loss = ((x_l_tilda - x_l) ** 2).mean()             #reconstruction loss
     err_dec = gamma * rec_loss - gan_loss 
     recon_loss_list.append(rec_loss.item())
     optim_D.zero_grad()
     err_dec.backward(retain_graph=True)
     optim_D.step()
     
+    #Training Encoder
     mean, logvar, rec_enc = gen(datav)
     x_l_tilda = discrim(rec_enc)[1]
     x_l = discrim(datav)[1]
     rec_loss = ((x_l_tilda - x_l) ** 2).mean()
-    prior_loss = 1 + logvar - mean.pow(2) - logvar.exp()
+    prior_loss = 1 + logvar - mean.pow(2) - logvar.exp()           #KL Divergence
     prior_loss = (-0.5 * torch.sum(prior_loss))/torch.numel(mean.data)
     prior_loss_list.append(prior_loss.item())
-    err_enc = prior_loss + 5*rec_loss
+    err_enc = prior_loss + beta*rec_loss
 
     optim_E.zero_grad()
     err_enc.backward(retain_graph=True)
@@ -97,22 +142,12 @@ for epoch in range(epochs):
 
   
    
-   
-   
-   
-    
-    
-  
-
-
-    
-
   b=gen(x_fixed)[2]
   b=b.detach()
   c=gen.decoder(z_fixed)
   c=c.detach()
-  show_and_save('MNISTrec_noise_epoch_%d.png' % epoch ,make_grid((c*0.5+0.5).cpu(),8))
-  show_and_save('MNISTrec_epoch_%d.png' % epoch ,make_grid((b*0.5+0.5).cpu(),8))
+  show_and_save('rec_noise_epoch_%d.png' % epoch ,make_grid((c*0.5+0.5).cpu(),8))
+  show_and_save('rec_epoch_%d.png' % epoch ,make_grid((b*0.5+0.5).cpu(),8))
 
 plot_loss(prior_loss_list)
 plot_loss(recon_loss_list)
