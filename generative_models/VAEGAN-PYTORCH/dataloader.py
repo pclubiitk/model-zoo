7d693b5b4238370b9248f6bdd08bdb450f17008e
Rishabh Dugaye <58358984+rishabhd786@users.noreply.github.com>
2020-05-20 10:16:03 +0530

    Update dataloader.py

--- generative_models/VAEGAN-PYTORCH/dataloader.py
+++ generative_models/VAEGAN-PYTORCH/dataloader.py
@@ -5,9 +5,17 @@ import torchvision.transforms as transforms
 import torchvision.utils as vutils
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def dataloader(batch_size):
-  dataroot="/content/drive/My Drive/celeba"
-  transform=transforms.Compose([ transforms.Resize(64),transforms.CenterCrop(64),transforms.ToTensor(),transforms.Normalize((0.5),(0.5))])
-  dataset=torchvision.datasets.MNIST(root=dataroot, train=True,transform=transform, download=True)
-  data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
-  return data_loader
+def dataloader(batch_size,dataroot,dataset_name,image_size):
+  if dataset_name == 'mnist':
+  
+    transform=transforms.Compose([ transforms.Resize(image_size),transforms.CenterCrop(image_size),transforms.ToTensor(),transforms.Normalize((0.5),(0.5))])
+    dataset=torchvision.datasets.MNIST(root=dataroot, train=True,transform=transform, download=True)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return data_loader
+  if dataset_name == 'cifar10':
+  
+    transform=transforms.Compose([ transforms.Resize(image_size),transforms.CenterCrop(image_size),transforms.ToTensor(),transforms.Normalize((0.5),(0.5))])
+    dataset=torchvision.datasets.CIFAR10(root=dataroot, train=True,transform=transform, download=True)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return data_loader
+  
