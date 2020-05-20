02e30acf8c36d30d3cf7d2ea993aebf5e8268c32
Rishabh Dugaye <58358984+rishabhd786@users.noreply.github.com>
2020-05-20 11:17:27 +0530

    Update dataloader.py

--- generative_models/VAEGAN-PYTORCH/dataloader.py
+++ generative_models/VAEGAN-PYTORCH/dataloader.py
@@ -12,6 +12,7 @@ def dataloader(batch_size,dataroot,dataset_name,image_size):
     dataset=torchvision.datasets.MNIST(root=dataroot, train=True,transform=transform, download=True)
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
     return data_loader
+  
   if dataset_name == 'cifar10':
   
     transform=transforms.Compose([ transforms.Resize(image_size),transforms.CenterCrop(image_size),transforms.ToTensor(),transforms.Normalize((0.5),(0.5))])
