a7636ed6c819209cb87b00dd8c5a7c43ac476f08
ashishpm <ashish.murali27@gmail.com>
2020-05-23 13:38:19 +0530

    Update files and compress gif

--- generative_models/VanillaGAN_TensorFlow/models.py
+++ generative_models/VanillaGAN_TensorFlow/models.py
@@ -3,14 +3,14 @@ from tensorflow.keras.layers import Dense, Dropout, Input, BatchNormalization,Le
 from tensorflow.keras.models import Model,Sequential
 from tensorflow.keras.optimizers import Adam
 
-def adam_optimizer():
-    return Adam(lr=0.0002, beta_1=0.5)
+def adam_optimizer(learning_rate,beta_1):
+    return Adam(lr=learning_rate, beta_1=beta_1)
 
-def create_generator():
+def create_generator(learning_rate,beta_1,encoding_dims):
 
     generator=Sequential()
 
-    generator.add(Dense(units=256,input_dim=100))
+    generator.add(Dense(units=256,input_dim=encoding_dims))
     generator.add(LeakyReLU(0.2))
     generator.add(BatchNormalization(momentum=0.8))
 
@@ -24,10 +24,10 @@ def create_generator():
 
     generator.add(Dense(units=784, activation='tanh'))
 
-    generator.compile(loss='binary_crossentropy', optimizer=adam_optimizer())
+    generator.compile(loss='binary_crossentropy', optimizer=adam_optimizer(learning_rate,beta_1))
     return generator
 
-def create_discriminator():
+def create_discriminator(learning_rate,beta_1):
 
     discriminator=Sequential()
 
@@ -45,13 +45,13 @@ def create_discriminator():
 
     discriminator.add(Dense(units=1, activation='sigmoid'))
 
-    discriminator.compile(loss='binary_crossentropy', optimizer=adam_optimizer())
+    discriminator.compile(loss='binary_crossentropy', optimizer=adam_optimizer(learning_rate,beta_1))
     return discriminator
 
-def create_gan(discriminator, generator):
+def create_gan(discriminator, generator,encoding_dims):
 
     discriminator.trainable=False
-    gan_input = Input(shape=(100,))
+    gan_input = Input(shape=(encoding_dims,))
     x = generator(gan_input)
     gan_output= discriminator(x)
     gan= Model(inputs=gan_input, outputs=gan_output)
