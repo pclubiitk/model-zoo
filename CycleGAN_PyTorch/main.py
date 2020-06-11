cc100518ccc310f17586b41fe368320c41ef8382
mridul911 <60569576+mridul911@users.noreply.github.com>
2020-06-11 22:04:51 +0530

    Add files via upload

--- CycleGAN_PyTorch/main.py
+++ CycleGAN_PyTorch/main.py
@@ -1,6 +1,7 @@
 import os
 from argparse import ArgumentParser
 import model as md
+from utils import *
 from utils import create_link
 
 # To get arguments from commandline
@@ -53,6 +54,194 @@ def main():
       model = md.cycleGAN(args)
       model.test(args)
 
+def train(self,args):
+        # For transforming the input image
+        transform = transforms.Compose(
+            [transforms.RandomHorizontalFlip(),
+             transforms.Resize((args.load_height,args.load_width)),
+             transforms.RandomCrop((args.crop_height,args.crop_width)),
+             transforms.ToTensor(),
+             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
 
+        dataset_dirs = utils.get_traindata_link(args.dataset_dir)
+
+        # Pytorch dataloader
+        a_loader = torch.utils.data.DataLoader(dsets.ImageFolder(dataset_dirs['trainA'], transform=transform), 
+                                                        batch_size=args.batch_size, shuffle=True, num_workers=4)
+        b_loader = torch.utils.data.DataLoader(dsets.ImageFolder(dataset_dirs['trainB'], transform=transform), 
+                                                        batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+        a_fake_sample = utils.Sample_from_Pool()
+        b_fake_sample = utils.Sample_from_Pool()
+
+        for epoch in range(self.start_epoch, args.epochs):
+
+            lr = self.g_optimizer.param_groups[0]['lr']
+            print('learning rate = %.7f' % lr)
+
+            for i, (a_real, b_real) in enumerate(zip(a_loader, b_loader)):
+                # step
+                step = epoch * min(len(a_loader), len(b_loader)) + i + 1
+
+                # Generator Computations
+                ##################################################
+
+                set_grad([self.Da, self.Db], False)
+                self.g_optimizer.zero_grad()
+
+                a_real = Variable(a_real[0])
+                b_real = Variable(b_real[0])
+                a_real, b_real = utils.cuda([a_real, b_real])
+
+                # Forward pass through generators
+                ##################################################
+                a_fake = self.Gab(b_real)
+                b_fake = self.Gba(a_real)
+
+                a_recon = self.Gab(b_fake)
+                b_recon = self.Gba(a_fake)
+
+                a_idt = self.Gab(a_real)
+                b_idt = self.Gba(b_real)
+
+                # Identity losses
+                ###################################################
+                a_idt_loss = self.L1(a_idt, a_real) * args.lamda * args.idt_coef
+                b_idt_loss = self.L1(b_idt, b_real) * args.lamda * args.idt_coef
+
+                # Adversarial losses
+                ###################################################
+                a_fake_dis = self.Da(a_fake)
+                b_fake_dis = self.Db(b_fake)
+
+                real_label = utils.cuda(Variable(torch.ones(a_fake_dis.size())))
+
+                a_gen_loss = self.MSE(a_fake_dis, real_label)
+                b_gen_loss = self.MSE(b_fake_dis, real_label)
+
+                # Cycle consistency losses
+                ###################################################
+                a_cycle_loss = self.L1(a_recon, a_real) * args.lamda
+                b_cycle_loss = self.L1(b_recon, b_real) * args.lamda
+
+                # Total generators losses
+                ###################################################
+                gen_loss = a_gen_loss + b_gen_loss + a_cycle_loss + b_cycle_loss + a_idt_loss + b_idt_loss
+
+                # Update generators
+                ###################################################
+                gen_loss.backward()
+                self.g_optimizer.step()
+
+
+                # Discriminator Computations
+                #################################################
+
+                set_grad([self.Da, self.Db], True)
+                self.d_optimizer.zero_grad()
+
+                # Sample from history of generated images
+                #################################################
+                a_fake = Variable(torch.Tensor(a_fake_sample([a_fake.cpu().data.numpy()])[0]))
+                b_fake = Variable(torch.Tensor(b_fake_sample([b_fake.cpu().data.numpy()])[0]))
+                a_fake, b_fake = utils.cuda([a_fake, b_fake])
+
+                # Forward pass through discriminators
+                ################################################# 
+                a_real_dis = self.Da(a_real)
+                a_fake_dis = self.Da(a_fake)
+                b_real_dis = self.Db(b_real)
+                b_fake_dis = self.Db(b_fake)
+                real_label = utils.cuda(Variable(torch.ones(a_real_dis.size())))
+                fake_label = utils.cuda(Variable(torch.zeros(a_fake_dis.size())))
+
+                # Discriminator losses
+                ##################################################
+                a_dis_real_loss = self.MSE(a_real_dis, real_label)
+                a_dis_fake_loss = self.MSE(a_fake_dis, fake_label)
+                b_dis_real_loss = self.MSE(b_real_dis, real_label)
+                b_dis_fake_loss = self.MSE(b_fake_dis, fake_label)
+
+                # Total discriminators losses
+                a_dis_loss = (a_dis_real_loss + a_dis_fake_loss)*0.5
+                b_dis_loss = (b_dis_real_loss + b_dis_fake_loss)*0.5
+
+                # Update discriminators
+                ##################################################
+                a_dis_loss.backward()
+                b_dis_loss.backward()
+                self.d_optimizer.step()
+
+                print("Epoch: (%3d) (%5d/%5d) | Gen Loss:%.2e | Dis Loss:%.2e" % 
+                                            (epoch, i + 1, min(len(a_loader), len(b_loader)),
+                                                            gen_loss,a_dis_loss+b_dis_loss))
+
+            # Override the latest checkpoint
+            #######################################################
+            utils.save_checkpoint({'epoch': epoch + 1,
+                                   'Da': self.Da.state_dict(),
+                                   'Db': self.Db.state_dict(),
+                                   'Gab': self.Gab.state_dict(),
+                                   'Gba': self.Gba.state_dict(),
+                                   'd_optimizer': self.d_optimizer.state_dict(),
+                                   'g_optimizer': self.g_optimizer.state_dict()},
+                                  '%s/latest.ckpt' % (args.checkpoint_dir))
+
+            # Update learning rates
+            ########################
+            self.g_lr_scheduler.step()
+            self.d_lr_scheduler.step()
+def test(args):
+
+    transform = transforms.Compose(
+        [transforms.Resize((args.crop_height,args.crop_width)),
+         transforms.ToTensor(),
+         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+
+    dataset_dirs = utils.get_testdata_link(args.dataset_dir)
+
+    a_test_data = dsets.ImageFolder(dataset_dirs['testA'], transform=transform)
+    b_test_data = dsets.ImageFolder(dataset_dirs['testB'], transform=transform)
+
+
+    a_test_loader = torch.utils.data.DataLoader(a_test_data, batch_size=args.batch_size, shuffle=True, num_workers=4)
+    b_test_loader = torch.utils.data.DataLoader(b_test_data, batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+    Gab = define_Gen(input_nc=3, output_nc=3, ngf=args.ngf, netG='resnet_9blocks', norm=args.norm, 
+                                                    use_dropout= not args.no_dropout, gpu_ids=args.gpu_ids)
+    Gba = define_Gen(input_nc=3, output_nc=3, ngf=args.ngf, netG='resnet_9blocks', norm=args.norm, 
+                                                    use_dropout= not args.no_dropout, gpu_ids=args.gpu_ids)
+
+    utils.print_networks([Gab,Gba], ['Gab','Gba'])
+
+    try:
+            ckpt = utils.load_checkpoint('%s/latest.ckpt' % (args.checkpoint_dir))
+        Gab.load_state_dict(ckpt['Gab'])
+            Gba.load_state_dict(ckpt['Gba'])
+    except:
+            print(' [*] No checkpoint!')
+
+
+    """ run """
+    a_real_test = Variable(iter(a_test_loader).next()[0], requires_grad=True)
+    b_real_test = Variable(iter(b_test_loader).next()[0], requires_grad=True)
+    a_real_test, b_real_test = utils.cuda([a_real_test, b_real_test])
+            
+
+    Gab.eval()
+    Gba.eval()
+
+    with torch.no_grad():
+        a_fake_test = Gab(b_real_test)
+        b_fake_test = Gba(a_real_test)
+        a_recon_test = Gab(b_fake_test)
+        b_recon_test = Gba(a_fake_test)
+
+    pic = (torch.cat([a_real_test, b_fake_test, a_recon_test, b_real_test, a_fake_test, b_recon_test], dim=0).data + 1) / 2.0
+
+    if not os.path.isdir(args.results_dir):
+        os.makedirs(args.results_dir)
+
+    torchvision.utils.save_image(pic, args.results_dir+'/sample.jpg', nrow=3)
 if __name__ == '__main__':
     main()
