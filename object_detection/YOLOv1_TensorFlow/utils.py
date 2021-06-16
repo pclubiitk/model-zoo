418513858e5630f7e947a102497bd6fe66e945d6
dirtbkr <p.r.bharwani@gmail.com>
2021-06-16 22:55:53 +0530

    Basic changes

--- object_detection/YOLOv1_TensorFlow/utils.py
+++ object_detection/YOLOv1_TensorFlow/utils.py
@@ -4,56 +4,64 @@ import keras.backend as K
 import cv2 as cv
 import numpy as np
 
-# We are going to define a custom layer , that will serve as the reshaping layer. 
+# We are going to define a custom layer , that will serve as the reshaping layer.
 class CustomReshapeLayer(tf.keras.layers.Layer):
-  def __init__(self, target_shape):
-    super(CustomReshapeLayer, self).__init__()
-    self.target_shape = tuple(target_shape)
+    def __init__(self, target_shape):
+        super(CustomReshapeLayer, self).__init__()
+        self.target_shape = tuple(target_shape)
 
-  def get_config(self):
-    config = super().get_config().copy()
-    config.update({
-        'target_shape': self.target_shape
-    })
-    return config
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({"target_shape": self.target_shape})
+        return config
 
-  def call(self, input):
-    # grids 7x7
-    S = [self.target_shape[0], self.target_shape[1]]
-    # classes
-    C = 20
-    # no of bounding boxes per grid
-    B = 2
-    # n_class_prob_elements is 7*7*20 = 980 | Represents the class probabilities of every grid cell .
-    n_class_prob_elements = S[0] * S[1] * C
-    # 
-    n_class_prob_and_prob_object = n_class_prob_elements + S[0] * S[1] * B
-    
-    # class probabilities
-    # taking the FIRST 'n_class_prob_elements' values and reshaping them into 7*7*20 tensor
-    class_probs = K.reshape( input[:, :n_class_prob_elements], (K.shape(input)[0],) + tuple([S[0], S[1], C]) )
-    class_probs = K.softmax(class_probs)
+    def call(self, input):
+        # grids 7x7
+        S = [self.target_shape[0], self.target_shape[1]]
+        # classes
+        C = 20
+        # no of bounding boxes per grid
+        B = 2
+        # n_class_prob_elements is 7*7*20 = 980 | Represents the class probabilities of every grid cell .
+        n_class_prob_elements = S[0] * S[1] * C
+        #
+        n_class_prob_and_prob_object = n_class_prob_elements + S[0] * S[1] * B
 
-    # confidence
-    # taking the rest of the elements and reshaping them into a 7*7*2 tensor
-    # These will represent the Pr(Object) of each grid cell. Notice we have two of these, since
-    # we are dealing with two anchor boxes per cell
-    confs = K.reshape(input[:, n_class_prob_elements:n_class_prob_and_prob_object], (K.shape(input)[0],) + tuple([S[0], S[1], B]))
-    confs = K.sigmoid(confs)
+        # class probabilities
+        # taking the FIRST 'n_class_prob_elements' values and reshaping them into 7*7*20 tensor
+        class_probs = K.reshape(
+            input[:, :n_class_prob_elements],
+            (K.shape(input)[0],) + tuple([S[0], S[1], C]),
+        )
+        class_probs = K.softmax(class_probs)
 
-    # boxes
-    # Now we are taking the last elements , i.e they will represent the bounding boxes parameters . 
-    # Each box will have 4 parameters to completely describe the box. 
-    # And thus we are reshaping them into a 7*7*8   |  8 because we have 2 bounding boxes per cell , so (4*2) params each cell
-    boxes = K.reshape(input[:, n_class_prob_and_prob_object:], (K.shape(input)[0],) + tuple([S[0], S[1], B * 4]))
-    boxes = K.sigmoid(boxes)
+        # confidence
+        # taking the rest of the elements and reshaping them into a 7*7*2 tensor
+        # These will represent the Pr(Object) of each grid cell. Notice we have two of these, since
+        # we are dealing with two anchor boxes per cell
+        confs = K.reshape(
+            input[:, n_class_prob_elements:n_class_prob_and_prob_object],
+            (K.shape(input)[0],) + tuple([S[0], S[1], B]),
+        )
+        confs = K.sigmoid(confs)
 
-    outputs = K.concatenate([class_probs, confs, boxes])
-    return outputs
+        # boxes
+        # Now we are taking the last elements , i.e they will represent the bounding boxes parameters .
+        # Each box will have 4 parameters to completely describe the box.
+        # And thus we are reshaping them into a 7*7*8   |  8 because we have 2 bounding boxes per cell , so (4*2) params each cell
+        boxes = K.reshape(
+            input[:, n_class_prob_and_prob_object:],
+            (K.shape(input)[0],) + tuple([S[0], S[1], B * 4]),
+        )
+        boxes = K.sigmoid(boxes)
 
-#Custom Reshape Layer END ************************************************************************************************
+        outputs = K.concatenate([class_probs, confs, boxes])
+        return outputs
 
-# Since the paper implements a custom learning rate schedule 
+
+# Custom Reshape Layer END ************************************************************************************************
+
+# Since the paper implements a custom learning rate schedule
 # Let us define a Custom Learning Rate Scheduler
 class CustomLearningRateScheduler(keras.callbacks.Callback):
     def __init__(self, schedule):
@@ -89,7 +97,8 @@ def lr_schedule(epoch, lr):
             return LR_SCHEDULE[i][1]
     return lr
 
-#Custom Learning Rate Scheduler END ********************************************************************************
+
+# Custom Learning Rate Scheduler END ********************************************************************************
 
 # Now let us define the loss function and the other functions required
 def xywh2minmax(xy, wh):
@@ -98,16 +107,22 @@ def xywh2minmax(xy, wh):
 
     return xy_min, xy_max
 
-test_done=False
+
+test_done = False
+
 
 def iou(pred_mins, pred_maxes, true_mins, true_maxes):
     global test_done
     if not test_done:
-      print("iou funciton : {} | {} | {} | {}".format(pred_mins, pred_maxes, true_mins, true_maxes) )
-      test_done=True
+        print(
+            "iou funciton : {} | {} | {} | {}".format(
+                pred_mins, pred_maxes, true_mins, true_maxes
+            )
+        )
+        test_done = True
     intersect_mins = K.maximum(pred_mins, true_mins)
     intersect_maxes = K.minimum(pred_maxes, true_maxes)
-    intersect_wh = K.maximum(intersect_maxes - intersect_mins, 0.)
+    intersect_wh = K.maximum(intersect_maxes - intersect_mins, 0.0)
     intersect_areas = intersect_wh[..., 0] * intersect_wh[..., 1]
 
     pred_wh = pred_maxes - pred_mins
@@ -120,6 +135,7 @@ def iou(pred_mins, pred_maxes, true_mins, true_maxes):
 
     return iou_scores
 
+
 def yolo_head(feats):
     # Dynamic implementation of conv dims for fully convolutional model.
     conv_dims = K.shape(feats)[1:3]  # assuming channels last
@@ -130,8 +146,7 @@ def yolo_head(feats):
 
     # TODO: Repeat_elements and tf.split doesn't support dynamic splits.
     # conv_width_index = K.repeat_elements(conv_width_index, conv_dims[1], axis=0)
-    conv_width_index = K.tile(
-        K.expand_dims(conv_width_index, 0), [conv_dims[0], 1])
+    conv_width_index = K.tile(K.expand_dims(conv_width_index, 0), [conv_dims[0], 1])
     conv_width_index = K.flatten(K.transpose(conv_width_index))
     conv_index = K.transpose(K.stack([conv_height_index, conv_width_index]))
     conv_index = K.reshape(conv_index, [1, conv_dims[0], conv_dims[1], 1, 2])
@@ -161,14 +176,22 @@ def yolo_loss(y_true, y_pred):
     label_xy, label_wh = yolo_head(_label_box)  # ? * 7 * 7 * 1 * 2, ? * 7 * 7 * 1 * 2
     label_xy = K.expand_dims(label_xy, 3)  # ? * 7 * 7 * 1 * 1 * 2
     label_wh = K.expand_dims(label_wh, 3)  # ? * 7 * 7 * 1 * 1 * 2
-    label_xy_min, label_xy_max = xywh2minmax(label_xy, label_wh)  # ? * 7 * 7 * 1 * 1 * 2, ? * 7 * 7 * 1 * 1 * 2
+    label_xy_min, label_xy_max = xywh2minmax(
+        label_xy, label_wh
+    )  # ? * 7 * 7 * 1 * 1 * 2, ? * 7 * 7 * 1 * 1 * 2
 
-    predict_xy, predict_wh = yolo_head(_predict_box)  # ? * 7 * 7 * 2 * 2, ? * 7 * 7 * 2 * 2
+    predict_xy, predict_wh = yolo_head(
+        _predict_box
+    )  # ? * 7 * 7 * 2 * 2, ? * 7 * 7 * 2 * 2
     predict_xy = K.expand_dims(predict_xy, 4)  # ? * 7 * 7 * 2 * 1 * 2
     predict_wh = K.expand_dims(predict_wh, 4)  # ? * 7 * 7 * 2 * 1 * 2
-    predict_xy_min, predict_xy_max = xywh2minmax(predict_xy, predict_wh)  # ? * 7 * 7 * 2 * 1 * 2, ? * 7 * 7 * 2 * 1 * 2
+    predict_xy_min, predict_xy_max = xywh2minmax(
+        predict_xy, predict_wh
+    )  # ? * 7 * 7 * 2 * 1 * 2, ? * 7 * 7 * 2 * 1 * 2
 
-    iou_scores = iou(predict_xy_min, predict_xy_max, label_xy_min, label_xy_max)  # ? * 7 * 7 * 2 * 1
+    iou_scores = iou(
+        predict_xy_min, predict_xy_max, label_xy_min, label_xy_max
+    )  # ? * 7 * 7 * 2 * 1
     best_ious = K.max(iou_scores, axis=4)  # ? * 7 * 7 * 2
     best_box = K.max(best_ious, axis=3, keepdims=True)  # ? * 7 * 7 * 1
 
@@ -186,18 +209,26 @@ def yolo_loss(y_true, y_pred):
     _predict_box = K.reshape(predict_box, [-1, 7, 7, 2, 4])
 
     label_xy, label_wh = yolo_head(_label_box)  # ? * 7 * 7 * 1 * 2, ? * 7 * 7 * 1 * 2
-    predict_xy, predict_wh = yolo_head(_predict_box)  # ? * 7 * 7 * 2 * 2, ? * 7 * 7 * 2 * 2
+    predict_xy, predict_wh = yolo_head(
+        _predict_box
+    )  # ? * 7 * 7 * 2 * 2, ? * 7 * 7 * 2 * 2
 
     box_mask = K.expand_dims(box_mask)
     response_mask = K.expand_dims(response_mask)
 
     box_loss = 5 * box_mask * response_mask * K.square((label_xy - predict_xy) / 448)
-    box_loss += 5 * box_mask * response_mask * K.square((K.sqrt(label_wh) - K.sqrt(predict_wh)) / 448)
+    box_loss += (
+        5
+        * box_mask
+        * response_mask
+        * K.square((K.sqrt(label_wh) - K.sqrt(predict_wh)) / 448)
+    )
     box_loss = K.sum(box_loss)
 
     loss = confidence_loss + class_loss + box_loss
 
     return loss
+
 
 # Loss Function END ***************************************************************************************************
 
@@ -208,11 +239,11 @@ def read(image_path, label):
     image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
     image_h, image_w = image.shape[0:2]
     image = cv.resize(image, (448, 448))
-    image = image / 255.
+    image = image / 255.0
 
     label_matrix = np.zeros([7, 7, 30])
     for l in label:
-        l = l.split(',')
+        l = l.split(",")
         l = np.array(l, dtype=np.int)
         xmin = l[0]
         ymin = l[1]
@@ -236,38 +267,36 @@ def read(image_path, label):
 
     return image, label_matrix
 
+
 # Read function END **************************************************************************************************************
 
 # Custom Data Generator
 
-class My_Custom_Generator(keras.utils.Sequence):
-  
-  def __init__(self, images, labels, batch_size):
-    self.images = images
-    self.labels = labels
-    self.batch_size = batch_size
-    
-  def __len__(self) :
-    return (np.ceil(len(self.images) / float(self.batch_size))).astype(np.int)
-  
-  def __getitem__(self, idx) :
-    batch_x = self.images[idx * self.batch_size : (idx+1) * self.batch_size]
-    batch_y = self.labels[idx * self.batch_size : (idx+1) * self.batch_size]
-    # print("batch_x:{}".format(batch_x))
-    train_image = []
-    train_label = []
 
-    for i in range(0, len(batch_x)):
-      img_path = batch_x[i]
-      label = batch_y[i]
-    #   print("prem test:{}".format(img_path))
-      image, label_matrix = read(img_path, label)
-      train_image.append(image)
-      train_label.append(label_matrix)
-    return np.array(train_image), np.array(train_label)
+class My_Custom_Generator(keras.utils.Sequence):
+    def __init__(self, images, labels, batch_size):
+        self.images = images
+        self.labels = labels
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return (np.ceil(len(self.images) / float(self.batch_size))).astype(np.int)
+
+    def __getitem__(self, idx):
+        batch_x = self.images[idx * self.batch_size : (idx + 1) * self.batch_size]
+        batch_y = self.labels[idx * self.batch_size : (idx + 1) * self.batch_size]
+        # print("batch_x:{}".format(batch_x))
+        train_image = []
+        train_label = []
+
+        for i in range(0, len(batch_x)):
+            img_path = batch_x[i]
+            label = batch_y[i]
+            #   print("prem test:{}".format(img_path))
+            image, label_matrix = read(img_path, label)
+            train_image.append(image)
+            train_label.append(label_matrix)
+        return np.array(train_image), np.array(train_label)
+
 
 # Custom Generator END **************************************************************************************************************
-
-
-# def get_final_bboxes(y_pred):
-#   visited = np.zeros((7,7))
