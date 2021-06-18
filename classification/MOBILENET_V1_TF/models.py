3a11d6228131df3d8c797062f14b3ddfbf693ec7
Rajarshi1001 <rajarshi20@iitk.ac.in>
2021-06-18 19:57:11 +0530

    Update models.py

--- classification/MOBILENET_V1_TF/models.py
+++ classification/MOBILENET_V1_TF/models.py
@@ -12,15 +12,15 @@ from tensorflow.keras.layers import (
 from tensorflow.keras.optimizers import Adam
 
 
-# ? This function is defined for the standard convolutions operations applied on the images
-# ? A standard convolution operation basically applies a kernel of dimensions Dk x Dk x M x N to the corresponding feature map  of dimensions
-# ? Df x Df x M to reduce it to a output feature map of dimensions Df x Df x N, here M denotes the number of input channels of the input feature map and
-# ? N denotes the number of output channels of the feature map obtained after each standard convolution operation. The overall computations
-# ? in a standard convolution is Dk x Dk x Df x Df x M x N since in each convolution operation Dk x Dk elemenst of the kernel are multiplied to each
-# ? element of the feature map and there are total of M x N filters.
+#  This function is defined for the standard convolutions operations applied on the images
+#  A standard convolution operation basically applies a kernel of dimensions Dk x Dk x M x N to the corresponding feature map  of dimensions
+#  Df x Df x M to reduce it to a output feature map of dimensions Df x Df x N, here M denotes the number of input channels of the input feature map and
+#  N denotes the number of output channels of the feature map obtained after each standard convolution operation. The overall computations
+#  in a standard convolution is Dk x Dk x Df x Df x M x N since in each convolution operation Dk x Dk elemenst of the kernel are multiplied to each
+#  element of the feature map and there are total of M x N filters.
 
-# ? The given function return the feature map tensor after applying the operation ollowed by batchnormalisation and RELU activation to
-# ? introduce non-linearity.
+#  The given function return the feature map tensor after applying the operation followed by batchnormalisation and RELU activation to
+#  introduce non-linearity.
 
 
 def standard_conv(X, filter, k_size, stride, width_mul):
@@ -32,8 +32,10 @@ def standard_conv(X, filter, k_size, stride, width_mul):
     return X
 
 
-# ? This function is defined for the depthwise convolutions applied in the mobilenet network which drastically reduces the computational costs.
-# ? in depthwise convoltuions,
+#  This function is defined for the depthwise convolutions applied in the mobilenet network which drastically reduces the computational costs.
+#  in depthwise convoltuions,filter is applied to each input channel, the first step is used to filter the inputs and then the output of the depthwise layers are combined 
+#  using pointwise convolutions(using 1x1 convolutions) to generate new features from the inputs followed by batchnormalisation and RELU activation function.
+#  This factorized convolutions drastically reduces the number of operations in standard convolutions.
 
 
 def depthwise_conv(X, filter, k_size, stride, width_mul):
@@ -69,11 +71,6 @@ def Mobilenet_v1(X):
     X = depthwise_conv(X, 1024, (3, 3), (1, 1), 1)
     X = GlobalAveragePooling2D()(X)
     X = Dense(units=10, activation="softmax")(X)
-
-    # input_tensor = Input(shape = (224,224,3))
-    # model = Model(inputs = X_input, outputs = X)
+    
     return X
 
-
-# uncomment this if you want to see the model params and the architecture.
-# model.summary()
