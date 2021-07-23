457fbfc16ce22c7670ce3001dfa9dadaa244ea91
dirtbkr <premr20@iitk.ac.in>
2021-07-24 00:44:51 +0530

    Did code formatting

--- classification/MLP-Mixer_tensorflow/main.py
+++ classification/MLP-Mixer_tensorflow/main.py
@@ -1,5 +1,3 @@
-
-
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -10,114 +8,63 @@ import os
 
 from load_dataset import (
     # Note you have to run 'load_dataset.py' atleast once so that you've downloaded the dataset
-    # This import is to load the database that we already downloaded, we're using the CIFAR-100 dataset. 
-    # Making use of keras to load the dataset. (check 'load_dataset.py')
+    # This import is to load the database that we already downloaded, we're using the CIFAR-100 dataset.
+    # Making use     of keras to load the dataset. (check 'load_dataset.py')
     x_train,
     x_test,
     y_test,
-    y_train
+    y_train,
 )
 
 from utils import (
     # Importing functions and other objects from utils.py
-    cifar_dict
+    cifar_dict,
 )
 
 from model import (
     # Function to build the model
-    build_model
+    train_model,
+    build_model,
+    train_bool,
+    test_display,
+    checkpoint_path,
 )
 
-# Parameters for the model
-train_bool = False    # If you want to train the model
-test_display = True   # To display a random image and the prediction from the test dataset
-checkpoint_path = '/home/merp/Desktop/model-zoo/classification/MLP-Mixer_tensorflow/models/cp.ckpt'
-num_classes = 100          
-input_shape = (32, 32, 3)   
-weight_decay = 0.0001
-batch_size = 128
-num_epochs = 50
-dropout_rate = 0.2
-image_size = 64  # We'll resize input images to this size.
-patch_size = 8  # Size of the patches to be extracted from the input images.
-num_patches = (image_size // patch_size) ** 2  # Size of the data array.
-embedding_dim = 256  # Number of hidden units.
-num_blocks = 4  # Number of blocks.
 
-print(f"Image size: {image_size} X {image_size} = {image_size ** 2}")
-print(f"Patch size: {patch_size} X {patch_size} = {patch_size ** 2} ")
-print(f"Patches per image: {num_patches}")
-print(f"Elements per patch (3 channels): {(patch_size ** 2) * 3}")
-
-
-
-def train_model(model):
-    # Create a learning rate scheduler callback.
-    reduce_lr = keras.callbacks.ReduceLROnPlateau(
-        monitor="val_loss", factor=0.5, patience=5
-    )
-    # Create an early stopping callback.
-    early_stopping = tf.keras.callbacks.EarlyStopping(
-        monitor="val_loss", patience=10, restore_best_weights=True
-    )
-
-    # Creating a model checkpoint callback, which saves it every 30 epochs
-    cp_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_path, 
-    verbose=1, 
-    save_weights_only=True,
-    save_freq=30)
-
-    #to get the latest checkpoint file
-    checkpoint_dir = os.path.dirname(checkpoint_path)
-    latest = tf.train.latest_checkpoint(checkpoint_dir)
-
-    # If we have a checkpoint file
-    if not ((latest)==None):
-      model.load_weights(latest)
-
-    # Fit the model.
-    history = model.fit(
-        x=x_train,
-        y=y_train,
-        batch_size=batch_size,
-        epochs=num_epochs,
-        validation_split=0.1,
-        callbacks=[early_stopping, reduce_lr, cp_callback],
-    )
-
-    _, accuracy, top_5_accuracy = model.evaluate(x_test, y_test)
-    print(f"Test accuracy: {round(accuracy * 100, 2)}%")
-    print(f"Test top 5 accuracy: {round(top_5_accuracy * 100, 2)}%")
-
-    # Return history to plot learning curves.
-    return history
-
+# Preparing the model
 my_model = build_model()
+# Printing the summary of the model
 my_model.summary()
 
-# The below line does training otherwise
+# The below line does training if train_bool is set to True [You can change this in model.py]
 if train_bool:
-    my_model = train_model(my_model)
+    my_model = train_model(my_model, checkpoint_path)
 
-#  To do direct testing without training
-tmp_checkpoint_dir = os.path.dirname("/home/merp/Desktop/model-zoo/classification/MLP-Mixer_tensorflow/models/cp.ckpt")
+#  Loading any checkpoint weights if we have any
+tmp_checkpoint_dir = os.path.dirname(
+    "/home/merp/Desktop/model-zoo/classification/MLP-Mixer_tensorflow/models/cp.ckpt"
+)
 latest_weights = tf.train.latest_checkpoint(tmp_checkpoint_dir)
 # If we have a saved weights in train folder
-if not ((latest_weights)==None):
+if not ((latest_weights) == None):
     my_model.load_weights(latest_weights)
 
 # To display the model's performance on a random image.
 if test_display:
     import random
-    i=random.randint(1,10000)
+
+    i = random.randint(1, 10000)
     plt.imshow(x_test[i])
     copy = x_test[i]
-    copy = copy[None,:,:,:]
-    y_pred = (my_model.predict(copy))
+    copy = copy[None, :, :, :]
+    y_pred = my_model.predict(copy)
     index = np.where(y_pred == np.amax(y_pred))
-    plt.title("[Ground Truth] : {}\n[Model's Prediction]: {}".format(cifar_dict[y_test[i][0]],cifar_dict[index[1][0]]))
+    plt.title(
+        "[Ground Truth] : {}\n[Model's Prediction]: {}".format(
+            cifar_dict[y_test[i][0]], cifar_dict[index[1][0]]
+        )
+    )
     print("i = {}".format(i))
     print("Y_test value [Ground Truth] : {}".format(cifar_dict[y_test[i][0]]))
-    print("Y_Pred value [Model's Prediction]: ",cifar_dict[index[1][0]])
+    print("Y_Pred value [Model's Prediction]: ", cifar_dict[index[1][0]])
     plt.show()
--- classification/MLP-Mixer_tensorflow/model.py
+++ classification/MLP-Mixer_tensorflow/model.py
@@ -2,20 +2,23 @@ import tensorflow.keras as keras
 import tensorflow as tf
 import tensorflow.keras.layers as layers
 import tensorflow_addons as tfa
-
-from load_dataset import (
-    x_train,
-    x_test,
-    y_test,
-    y_train
-)
-
+import os
 
 # Parameters for the model
-positional_encoding=False
-learning_rate = 200
-num_classes = 100          
-input_shape = (32, 32, 3)   
+train_bool = False  # If you want to train the model
+test_display = (
+    True  # To display a random image and the prediction from the test dataset
+)
+
+# *** NOTE : CHANGE YOUR CHECKPOINT DIRECTORY ACCORDINGLY ***
+checkpoint_path = (
+    "/home/merp/Desktop/model-zoo/classification/MLP-Mixer_tensorflow/models/cp.ckpt"
+)
+# ***********************************************************
+positional_encoding = False
+num_classes = 100
+learning_rate = 1e-6
+input_shape = (32, 32, 3)
 weight_decay = 0.0001
 batch_size = 128
 num_epochs = 50
@@ -25,6 +28,9 @@ patch_size = 8  # Size of the patches to be extracted from the input images.
 num_patches = (image_size // patch_size) ** 2  # Size of the data array.
 embedding_dim = 256  # Number of hidden units.
 num_blocks = 4  # Number of blocks.
+
+from load_dataset import x_train, x_test, y_test, y_train
+
 
 def convert_to_patches(input):
     batch_size = tf.shape(input)[0]
@@ -38,6 +44,7 @@ def convert_to_patches(input):
     patch_dims = patches.shape[-1]
     patches = tf.reshape(patches, [batch_size, num_patches, patch_dims])
     return patches
+
 
 class Patches(layers.Layer):
     def __init__(self, patch_size, num_patches):
@@ -100,10 +107,12 @@ class MLPMixerLayer(layers.Layer):
         x = x + mlp2_outputs
         return x
 
+
 def compile_model(model):
     # Create Adam optimizer with weight decay.
     optimizer = tfa.optimizers.AdamW(
-        learning_rate=learning_rate, weight_decay=weight_decay,
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
     )
     # Compile the model.
     model.compile(
@@ -117,9 +126,54 @@ def compile_model(model):
     model.summary()
     return model
 
+
+def train_model(model, checkpoint_path):
+    # Create a learning rate scheduler callback.
+    reduce_lr = keras.callbacks.ReduceLROnPlateau(
+        monitor="val_loss", factor=0.5, patience=5
+    )
+    # Create an early stopping callback.
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor="val_loss", patience=10, restore_best_weights=True
+    )
+
+    # Creating a model checkpoint callback, which saves it every 30 epochs
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_path, verbose=1, save_weights_only=True, save_freq=30
+    )
+
+    # To get the latest checkpoint file
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+
+    # Performing a check if we have a checkpoint file
+    if not ((latest) == None):
+        model.load_weights(latest)
+
+    # Fit the model.
+    trained_model = model.fit(
+        x=x_train,
+        y=y_train,
+        batch_size=batch_size,
+        epochs=num_epochs,
+        validation_split=0.1,
+        callbacks=[early_stopping, reduce_lr, cp_callback],
+    )
+
+    _, accuracy, top_5_accuracy = model.evaluate(x_test, y_test)
+    print(f"Test accuracy: {round(accuracy * 100, 2)}%")
+    print(f"Test top 5 accuracy: {round(top_5_accuracy * 100, 2)}%")
+
+    # Return history to plot learning curves.
+    return trained_model
+
+
 def build_model():
     mlpmixer_blocks = keras.Sequential(
-        [MLPMixerLayer(num_patches, embedding_dim, dropout_rate) for _ in range(num_blocks)]
+        [
+            MLPMixerLayer(num_patches, embedding_dim, dropout_rate)
+            for _ in range(num_blocks)
+        ]
     )
 
     inputs = layers.Input(shape=input_shape)
@@ -159,7 +213,6 @@ def build_model():
     logits = layers.Dense(num_classes)(representation)
     # Create the Keras model.
     mlpmixer_classifier = keras.Model(inputs=inputs, outputs=logits)
-
 
     # mlpmixer_classifier = build_classifier(mlpmixer_blocks)
     my_model = compile_model(mlpmixer_classifier)
